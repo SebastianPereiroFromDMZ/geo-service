32ee48af615f9295d67764a110385db68a8f51f7
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-01-30 23:34:41 +0300

    commit3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
@@ -19,5 +23,8 @@ public class Main {
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
         messageSender.send(headers);
+
+
+
     }
 }
--- src/test/java/GeoServiceImplTests.java
+++ src/test/java/GeoServiceImplTests.java
@@ -1,61 +1,37 @@
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GeoServiceImplTests {
 
+
     @ParameterizedTest
-    @ValueSource(strings = {"172.123.12.19", "172."})
-    void testLocationByIp(String ip) {
-        Location expended = new Location("Moscow", Country.RUSSIA, null, 0);
+    @MethodSource("sourceAdd")
+    void testLocationByIp(Location expended, String ip) {
         GeoService geoService = new GeoServiceImpl();
         Location location = geoService.byIp(ip);
         assertEquals(expended, location);
     }
 
-    public static Location location_1() {
-        return new Location(null, null, null, 0);
-    }
-
-    public static Location location_2() {
-        return new Location("Moscow", Country.RUSSIA, "Lenina", 15);
-    }
-
-    public Location location_3() {
-        return new Location("New York", Country.USA, " 10th Avenue", 32);
-    }
-
-    public Location location_4() {
-        return new Location("Moscow", Country.RUSSIA, null, 0);
-    }
-
-    public static Object[] location_5() {
-        return new Location[]{new Location("New York", Country.USA, null, 0)};
-    }
-
-    public Location location_6() {
-        return null;
+    public static Stream<Arguments> sourceAdd() {
+        return Stream.of(Arguments.of(new Location(null, null, null, 0), "127.0.0.1"),
+                Arguments.of(new Location("Moscow", Country.RUSSIA, "Lenina", 15), "172.0.32.11"),
+                Arguments.of(new Location("New York", Country.USA, " 10th Avenue", 32), "96.44.183.149"),
+                Arguments.of(new Location("Moscow", Country.RUSSIA, null, 0), "172."),
+                Arguments.of(new Location("New York", Country.USA, null, 0), "96."));
     }
 
 
-    @ParameterizedTest
-    @MethodSource("location_5")
-    void testLocationByIp_2(Location expected) {
-        String ip = "96.";
-        GeoService geoService = new GeoServiceImpl();
-        Location location = geoService.byIp(ip);
-        //assertEquals(expected, location);
 
 
-
-    }
 
 }
