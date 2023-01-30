b0cde53798725d5c100775811df224acde5bb6ee
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-01-30 23:46:34 +0300

    commit4

--- src/test/java/LocalizationServiceImplTests.java
+++ src/test/java/LocalizationServiceImplTests.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static ru.netology.entity.Country.RUSSIA;
+
+
 public class LocalizationServiceImplTests {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Добро пожаловать"})
+    void testLocale(String expected){
+        LocalizationService localizationService = new LocalizationServiceImpl();
+        String result = localizationService.locale(RUSSIA);
+        assertEquals(expected,result);
+    }
 }
